4cd8f70f92abea79a74a745b4bfebb09767dbcc0
Ashish Mukherjee <ashish.mukherjee@gmail.com>
2016-03-26 16:08:50 +0530

    yapf fixes

--- www/async_client.py
+++ www/async_client.py
@@ -1,17 +1,23 @@
 import pika
 import uuid
 
+
 class AsyncClient():
-    QUEUE_NAME = 'rpc_queue'
+    def __init__(self):
+        QUEUE_NAME = 'rpc_queue'
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=QUEUE_NAME, durable=False)
 
-	def __init__(self):
-		self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-		self.channel 	= self.connection.channel()
-		self.channel.queue_declare(queue=QUEUE_NAME, durable=False)
+    def call(self, lkey, msg, cust, filename):
+        QUEUE_NAME = 'rpc_queue'
+        q_entry = ":".join([lkey, cust, filename, msg])
+        return self.channel.basic_publish(
+            exchange='',
+            routing_key=QUEUE_NAME,
+            properties=pika.BasicProperties(delivery_mode=2),
+            body=q_entry)
 
-	def call(self,lkey,msg,cust,filename):
-		q_entry = ":".join([ lkey, cust, filename, msg ]) 
-		return self.channel.basic_publish(exchange='', routing_key=QUEUE_NAME, properties=pika.BasicProperties(delivery_mode=2), body=q_entry)
-
-	def close():
-		self.connection.close()
+    def close():
+        self.connection.close()
