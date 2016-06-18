3fbb6f431d95062c86226ddf1e0c29d99f7df90d
Ashish Mukherjee <ashish.mukherjee@gmail.com>
2016-06-18 13:45:46 +0000

    compile_java.sh edited online with Bitbucket

--- scripts/compile_java.sh
+++ scripts/compile_java.sh
@@ -1,16 +1,6 @@
-# Copy jar files to specified location:
-#	
-#	rabbitmq-client.jar -> textalysis/nlp_server
-# 	opennlp-tools-1.5.3.jar -> textalysis/nlp_server/lib
-# 	slf4j-api-1.6.4.jar -> /usr/share/java
-#	slf4j-simple-1.6.4.jar -> /usr/share/java
-# 	commons-daemon.jar -> /usr/share/java
-#	
-# Run following from textalysis/nlp_server
+export CLASSPATH="./libs/commons-daemon.jar:./libs/opennlp-maxent-3.0.1.jar:./libs/opennlp-tools-1.5.3.jar:./libs/rabbitmq-client.jar:./libs/slf4j-api-1.6.4.jar:./libs/slf4j-simple-1.6.4.jar:./dev"
 
-javac ./dev/com/efficientmail/Meter.java
-javac ./dev/com/efficientmail/TextClean.java
-javac -cp ./lib/opennlp-tools-1.5.3.jar ./dev/com/efficientmail/SentiClassifyLoader.java
-javac -cp ./lib/opennlp-tools-1.5.3.jar:/usr/share/java/slf4j-api-1.6.4.jar:./dev ./dev/com/efficientmail/SentiClassify.java
-javac -cp ./dev:/usr/share/java/slf4j-api-1.6.4.jar ./dev/com/efficientmail/ThemeDetect.java
-javac -cp ./rabbitmq-client.jar:./dev:/usr/share/java/commons-daemon.jar RabbitServer.java
+for i in `echo ./dev/com/efficientmail/*.java RabbitServer.java`
+do
+  javac $i
+done
