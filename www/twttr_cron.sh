df753e4748b1761202f70aff553f5518e141de62
Ashish Mukherjee <ashish.mukherjee@gmail.com>
2016-08-31 19:05:04 +0200

    update: fetch tweets script path

--- www/twttr_cron.sh
+++ www/twttr_cron.sh
@@ -2,6 +2,6 @@
 exists=$(crontab -l | grep -v "^(#|$)" | grep -qw "${1}"; echo $?)
 if [ ${exists} = 1 ]
 then
-  job="*/10 * * * * ${1}"
+  job="*/10 * * * * python ~/production/textalysis/www/twitter_srch.py ${1}"
   echo "$(crontab -l; echo "${job}")" | crontab -
 fi
